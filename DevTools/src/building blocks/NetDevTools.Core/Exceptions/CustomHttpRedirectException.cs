c35aca3b04cc827a7647cd4571139d579bc1570f
Filipe Gato <filipebertoldi@hotmail.com>
2022-05-06 00:00:30 -0300

    News custom exceptions

---
+++ DevTools/src/building blocks/NetDevTools.Core/Exceptions/CustomHttpRedirectException.cs
@@ -0,0 +1,15 @@
+﻿namespace NetDevTools.Core.Exceptions
+{
+    public class CustomHttpRedirectException : Exception
+    {
+        public string RedirectUrl { get; private set; }
+        public CustomHttpRedirectException() { }
+
+        public CustomHttpRedirectException(string redirectUrl, string message)
+            : base(message)
+        {
+            RedirectUrl = redirectUrl;
+        }
+
+    }
+}
---
+++ DevTools/src/building blocks/NetDevTools.Core/Exceptions/CustomHttpRequestException.cs
@@ -0,0 +1,11 @@
+﻿namespace NetDevTools.Core.Exceptions
+{
+    public class CustomHttpRequestException : Exception
+    {
+        public CustomHttpRequestException() { }
+
+        public CustomHttpRequestException(string message)
+            : base(message) { }
+
+    }
+}
