6cd5c955bf3c653a279535d6cb1f29f509b47b41
Filipe Gato <filipebertoldi@hotmail.com>
2022-05-09 18:06:14 -0300

    CommandHandler

---
+++ DevTools/src/building blocks/NetDevTools.Core/Messages/CommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
+using NetDevTools.Core.Data;
+
+namespace NetDevTools.Core.Messages
+{
+    public abstract class CommandHandler
+    {
+
+        protected ValidationResult ValidationResult;
+
+        protected CommandHandler()
+        {
+            ValidationResult = new ValidationResult();
+        }
+
+        protected void AdicionarErro(string mensagem)
+        {
+            ValidationResult.Errors.Add(new ValidationFailure(string.Empty, mensagem));
+        }
+
+        protected async Task<ValidationResult> SalvarBanco(IUnitOfWork uow)
+        {
+            try
+            {
+                if (!await uow.Commit())
+                {
+                    AdicionarErro("Houve um erro ao persistir os dados!");
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new Exception("Ops! Algo deu errado ao processar seu pedido. Estamos trabalhando para corrigir o problema");
+            }
+            catch
+            {
+                throw new Exception($"Ops! Algo deu errado ao processar sua requisição. Tente novamente mais tarde");
+            }
+
+            return ValidationResult;
+        }
+    }
+}
