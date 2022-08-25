b86b26dd4c84fcec3a371b58505e817edc8a5037
Filipe Gato <filipebertoldi@hotmail.com>
2022-08-25 14:31:44 -0300

    JsonIgnore validation result

--- DevTools/src/building blocks/NetDevTools.Core/Messages/Command.cs
+++ DevTools/src/building blocks/NetDevTools.Core/Messages/Command.cs
@@ -1,10 +1,12 @@
 ﻿using FluentValidation.Results;
 using MediatR;
+using System.Text.Json.Serialization;
 
 namespace NetDevTools.Core.Messages
 {
     public abstract class Command : Event, IRequest<ValidationResult>
     {
+        [JsonIgnore]
         public ValidationResult ValidationResult { get; set; }
 
         protected Command()
